f48009f099fab206ede9eea9fb81f3828301bcf0
rukaiah-edhah <rukaiah.edhah@yahoo.com>
2024-05-27 21:12:37 -0400

    Update: Fixed Tailwind CSS styling

--- ijele/components/UserAuth.tsx
+++ ijele/components/UserAuth.tsx
@@ -2,20 +2,27 @@ import {
     RegisterLink,
     LoginLink,
     LogoutLink,
-} from "@kinde-oss/kinde-auth-nextjs/components";
-
-export default function UserAuth() {
+  } from "@kinde-oss/kinde-auth-nextjs/components";
+  
+  export default function UserAuth() {
     return (
+      <div>
         <div>
-            <div>
-                <LoginLink>Log in</LoginLink>
-            </div>
-            <div>
-                <RegisterLink>Register</RegisterLink>
-            </div>
-            <div>
-                <LogoutLink>Log out</LogoutLink>
-            </div>
+          <LoginLink>
+            <button className="btn mb-1 mt-1">Log in</button>
+          </LoginLink>
         </div>
+        <div>
+          <RegisterLink>
+            <button className="btn mb-1 mt-1">Register</button>
+          </RegisterLink>
+        </div>
+        <div>
+          <LogoutLink>
+            <button className="btn mb-1 mt-1">Log out</button>
+          </LogoutLink>
+        </div>
+      </div>
     );
-}
+  }
+  
