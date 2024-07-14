1fb1680ff892592b779cf6265bf1ffac152fc8c8
rukaiah-edhah <rukaiah.edhah@yahoo.com>
2024-07-14 19:25:34 -0400

    Flight offer price api is working now

--- ijele/app/api/flights/price/route.ts
+++ ijele/app/api/flights/price/route.ts
@@ -3,16 +3,8 @@ import getFlightOffersPrice from "@/lib/flight/getFlightOffersPricing";
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
   try {
-    const { flightOffers } = await request.json();
-
-    if (!flightOffers || !Array.isArray(flightOffers)) {
-      return NextResponse.json(
-        { error: "Invalid or missing flight offers" },
-        { status: 400 }
-      );
-    }
-
-    const pricingResponse = await getFlightOffersPrice(flightOffers);
+    const flightOffersData = await request.json();
+    const pricingResponse = await getFlightOffersPrice(flightOffersData);
     return NextResponse.json(pricingResponse);
   } catch (error: any) {
     console.error("Error fetching flight offers pricing:", error);
