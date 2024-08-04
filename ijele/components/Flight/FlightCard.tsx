f77308d4a5cf04a3519f2f18113a19fec5844edf
ty <tbryson1024@gmail.com>
2024-08-03 23:47:33 -0500

    adjusting flight page to look like mockup

--- ijele/components/Flight/FlightCard.tsx
+++ ijele/components/Flight/FlightCard.tsx
@@ -42,29 +42,32 @@ const FlightCard: React.FC<FlightCardProps> = ({
 
   return (
     <div className="flex flex-col mb-4 shadow-lg overflow-hidden rounded-tr-[25%] border bg-ijele_navy">
-      <div className="flex items-center p-4 bg-red-200 border-black">
+      <div className="flex items-center p-4 bg-white border-black">
         <img src={logo} alt={`${airline} logo`} className="w-12 h-12 mr-4 rounded-full border" />
         <div className="flex-1">
           <div className="flex justify-between">
             <div>
-              <p className="text-lg font-semibold">{departureTime} - {arrivalTime}</p>
-              <p className="text-sm text-gray-500">{stops}</p>
+              <p className="text-lg font-junge">{departureTime} - {arrivalTime}</p>
+            </div>
+            
+            <div>
+              <p className="flex-1 text-center text-sm text-gray-500 font-junge">{stops}</p>
             </div>
             <div className="text-right">
-              <p className="text-sm text-gray-500"><i className="fa-solid fa-suitcase-rolling"></i>"</p>
+              <p className="text-sm text-gray-500"><i className="fa-solid fa-suitcase-rolling"></i></p>
             </div>
           </div>
         </div>
       </div>
-      <div className="flex items-center justify-between p-4 bg-ijele_navy text-white">
+      <div className="flex items-center justify-between p-4 bg-ijele_navy">
         <div className="flex-1 text-left">
-          <p className="text-sm">{airline}</p>
+          <p className="text-sm text-ijele_lightTeal font-kite_one italic">{airline}</p>
         </div>
         <div className="flex-1 text-center">
-          <p className="text-sm">{route}</p>
+          <p className="text-sm text-ijele_lightTeal font-kite_one italic">{route}</p>
         </div>
         <div className="flex-1 text-right">
-          <p className="text-lg font-semibold">{currency}{price}</p>
+          <p className="text-lg text-white font-inter">{currency}{price}</p>
         </div>
         <button onClick={toggleExpansion} className="text-sm text-ijele_gold focus:outline-none ml-4">
           {isExpanded ? '▲' : '▼'}
