e3c441c71137f89bfb72285ec96842082fc6ed5e
ty <tbryson1024@gmail.com>
2024-08-17 16:00:53 -0500

    font style and color

--- ijele/components/SlideText/SlideText.tsx
+++ ijele/components/SlideText/SlideText.tsx
@@ -9,11 +9,11 @@ const SlideText: React.FC<SlideTextProps> = ({ textOnLeft = true, text }) => {
   return (
     <div className="flex justify-center items-center w-full min-h-[100px]">
       {textOnLeft ? (
-        <h1 className="text-2xl font-bold animate-slideInBottom max-h-[100px] overflow-hidden flex items-center">
+        <h1 className="text-2xl font-junge text-ijele_teal font-bold animate-slideInBottom max-h-[100px] overflow-hidden flex items-center">
           {text}
         </h1>
       ) : (
-        <h1 className="text-2xl font-bold animate-slideInBottom max-h-[100px] overflow-hidden flex items-center">
+        <h1 className="text-2xl font-junge text-ijele_teal font-bold animate-slideInBottom max-h-[100px] overflow-hidden flex items-center">
           {text}
         </h1>
       )}
