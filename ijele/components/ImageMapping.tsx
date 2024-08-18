cd251e98f5f97a9afbc71d552cce8625c158ded6
unknown <cynthiaobieke@gmail.com>
2024-08-18 19:52:52 -0400

    new images for hotel

--- ijele/components/ImageMapping.tsx
+++ ijele/components/ImageMapping.tsx
@@ -1,13 +1,13 @@
 import { Iimg } from "./SearchPage/componentTypes";
 
-export const hotelCarouselImages = 
-     ["/Images/hotel/beach-bungalo.webp",
-    "/Images/hotel/high-quality-hotel-stars.jpg",
-    "/Images/hotel/hotel-lobby.jpg",
-    "/Images/hotel/inside_room.webp",
-    "/Images/hotel/neat-hotel-room.jpg",
-    "/Images/hotel/poolside-resort.webp",
-    "/Images/hotel/hotel_w_stars.jpg",]
+export const hotelCarouselImages =
+    ["/Images/hotel/beach-bungalo.webp",
+        "/Images/hotel/high-quality-hotel-stars.jpg",
+        "/Images/hotel/hotel-lobby.jpg",
+        "/Images/hotel/inside_room.webp",
+        "/Images/hotel/neat-hotel-room.jpg",
+        "/Images/hotel/poolside-resort.webp",
+        "/Images/hotel/hotel_w_stars.jpg",]
 
 
 export const flightCarouselImages = [
@@ -17,4 +17,23 @@ export const flightCarouselImages = [
     "/Images/flight/passportMisc.jpg",
     "/Images/flight/singapore2.jpg",
     "/Images/flight/peopleOnDaBeach.jpg"
+]
+
+export const hotelResultImages = [
+    "/Images/hotel/results/bed-hotelsroom.jpg",
+    "/Images/hotel/results/dolce-vita-sunshine-resort.jpg",
+    "/Images/hotel/results/genericHotel.jpg",
+    "/Images/hotel/results/honoluluPool.jpg",
+    "/Images/hotel/results/hotelpoolside.jpg",
+    "/Images/hotel/results/luxury-summer-resort.jpg",
+    "/Images/hotel/results/premiumlobby.jpg",
+    "/Images/hotel/results/resort1.jpg",
+    "/Images/hotel/results/resortLake.jpg",
+    "/Images/hotel/results/resortPretty.jpg",
+    "/Images/hotel/results/the-maybourne-beverly-hills-exterior.jpg",
+    "/Images/hotel/results/vancouver-lobby.jpg"
+]
+
+export const hotelQuotes = [
+    
 ]
