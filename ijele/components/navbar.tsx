3ee327c16a0c33ce77b51fd50153391bb87e154f
unknown <cynthiaobieke@gmail.com>
2024-08-20 22:16:05 -0400

    dashboard basic look

--- ijele/components/navbar.tsx
+++ ijele/components/navbar.tsx
@@ -11,7 +11,7 @@ type NavbarProps = {
   currentPage: string,
 }
 
-const navlist = ["Home", "Search", "Cart", "Dashboard","About"]
+const navlist = ["Home", "Search", "Cart", "Dashboard", "About"]
 
 
 const Navbar = (props: NavbarProps) => {
@@ -20,13 +20,13 @@ const Navbar = (props: NavbarProps) => {
   // State to track the currently expanded section
   const [expandedSection, setExpandedSection] = useState<boolean>(false);
   // Function to toggle the expansion of a specific section
-const toggleExpansion = (section: boolean) => {
-  setExpandedSection(section)
-};
-  
+  const toggleExpansion = (section: boolean) => {
+    setExpandedSection(section)
+  };
+
   // Toggle mobile menu visibility
   const toggleMenu = () => setIsOpen(!isOpen);
-  
+
   // Toggle user menu visibility
   const toggleUserMenu = () => setUserMenuOpen(!userMenuOpen);
 
@@ -34,8 +34,8 @@ const toggleExpansion = (section: boolean) => {
     return (
       <div className="dropdown dropdown-hover" onMouseOver={() => toggleExpansion(true)} onMouseLeave={() => toggleExpansion(false)}>
         <div className="navbarLink">Search</div>
-          {expandedSection ?
-        <ul className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+        {expandedSection ?
+          <ul className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
             <div>
               {currentPage === "Flight" ? <div>
                 <li><a href='/Flight' className='navbarLink text-ijele_teal border-b-2'>Flight</a></li>
@@ -45,28 +45,29 @@ const toggleExpansion = (section: boolean) => {
                   <li><a href='/Flight' className='navbarLink'>Flight</a></li>
                   <li><a href='/Hotel' className='navbarLink text-ijele_teal border-b-2'>Hotel</a></li>
                 </div>}
-            </div> 
-        </ul>
-            : null
-          }
+            </div>
+          </ul>
+          : null
+        }
       </div>
     )
   }
   function buildNavLinks(currentPage: string) {
     const links: JSX.Element[] = [];
     for (let i = 0; i < navlist.length; i++) {
-      if (navlist[i]==="Search"){
-          links.push(buildSearchDrpDwn(currentPage))}
+      if (navlist[i] === "Search") {
+        links.push(buildSearchDrpDwn(currentPage))
+      }
       else {
         const path = navlist[i] === "Home" ? "/" : `/${navlist[i]}`;
         const className = currentPage === navlist[i]
           ? 'navbarLink text-ijele_deepGold border-b-2'
           : 'navbarLink';
-  
+
         links.push(<a href={path} className={className}>{navlist[i]}</a>);
       }
     }
-  
+
     return links;
   }
 
@@ -74,7 +75,7 @@ const toggleExpansion = (section: boolean) => {
 
   return (
     <>
-      <nav className="">
+      <nav className="pb-6">
         <div className="mx-auto max-w-10xl px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -110,8 +111,13 @@ const toggleExpansion = (section: boolean) => {
               </button>
             </div>
             {/* logo container */}
-            <div className="logo-container flex-shrink-0 flex justify-center items-center h-full w-1/2">
-            <Image className="logo object-contained" src="/Images/logo.png" alt="ijele logo" width={100} height={100} />
+            <div className="relative logo-container flex-shrink-0 flex justify-center items-center h-full w-1/2">
+              <Image className="logo object-contained" src="/Images/logo.png" alt="ijele logo" width={100} height={100} />
+              <div className='absolute inset-x-0 top-24 flex items-center' >
+                <svg viewBox="20 3 300 12" xmlns="http://www.w3.org/2000/svg">
+                  <line x1="10" y1="3" x2="700" y2="3" stroke-dasharray="4" stroke='#CF903F' /></svg>
+                {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" width='3%'><path d="M482.3 192c34.2 0 93.7 29 93.7 64c0 36-59.5 64-93.7 64l-116.6 0L265.2 495.9c-5.7 10-16.3 16.1-27.8 16.1l-56.2 0c-10.6 0-18.3-10.2-15.4-20.4l49-171.6L112 320 68.8 377.6c-3 4-7.8 6.4-12.8 6.4l-42 0c-7.8 0-14-6.3-14-14c0-1.3 .2-2.6 .5-3.9L32 256 .5 145.9c-.4-1.3-.5-2.6-.5-3.9c0-7.8 6.3-14 14-14l42 0c5 0 9.8 2.4 12.8 6.4L112 192l102.9 0-49-171.6C162.9 10.2 170.6 0 181.2 0l56.2 0c11.5 0 22.1 6.2 27.8 16.1L365.7 192l116.6 0z" /></svg>*/}            
+                </div> 
             </div>
             {/* navigation (desktop) */}
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-end">
