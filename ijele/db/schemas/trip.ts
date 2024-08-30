2256064537aca9f13d6cb88a5389d46274f44556
unknown <cynthiaobieke@gmail.com>
2024-08-30 17:42:42 -0400

    routing

--- ijele/db/schemas/trip.ts
+++ ijele/db/schemas/trip.ts
@@ -1,32 +1,20 @@
-import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { uuid, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { usersTable } from './users';
 
 
 export const tripTable = pgTable('trip', {
-    tripId: serial('trip_id').primaryKey(),
+    tripId: serial('id').primaryKey(),
+    owner_id: uuid('owner').notNull().references(()=> usersTable.id),
     tripTitle: text('title'),
     location: text('location'),
     description: text('description'),
     tripImage: text('tripImage'),
     people: text('people'),
-    accom:
-    transport:
-    createdAt: timestamp('created_at').notNull().defaultNow(),
-    owner_id:
+    accom: text('hotels'),
+    transport: text('flights'),
+    createdAt: timestamp('created_at').notNull().defaultNow()
 })
 
-// public.trip (
-//     "tripId" uuid not null default gen_random_uuid (),
-//     "tripTitle" text null default ''::text,
-//     location text[] null default '{""}'::text[],
-//     description text null default ''::text,
-//     "tripImage" text null,
-//     people text[] null,
-//     accom json[] null,
-//     transport json[] null,
-//     created_at timestamp with time zone not null default now(),
-//     owner_id text null,
-//     constraint trip_pkey primary key ("tripId"),
-//     constraint trip_owner_id_key unique (owner_id),
-//     constraint trip_owner_id_fkey foreign key (owner_id) references user_profile (user_id)
-//   ) tablespace pg_default;
-// No file chosen
+export type InsertTrip = typeof tripTable.$inferInsert;
+export type SelectTrip = typeof tripTable.$inferSelect;
+
