02ba1a5da038d0c853e449d540026a1f94feae91
JenniferLarsen <djlarsen1999@gmail.com>
2024-09-02 15:54:21 -0500

    Added testing message to Hotel Payment Info section and alert to page.

--- ijele/components/HotelDetailsPage/PaymentInfoForm.tsx
+++ ijele/components/HotelDetailsPage/PaymentInfoForm.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 interface PaymentInfoFormProps {
   paymentInfo: {
     vendorCode: string;
@@ -11,10 +13,21 @@ interface PaymentInfoFormProps {
 const PaymentInfoForm: React.FC<PaymentInfoFormProps> = ({
   paymentInfo,
   handlePaymentChange,
+  
 }) => {
+  const [showAlert, setShowAlert] = useState(true);
+
+  useEffect(() => {
+    if (showAlert) {
+      alert("Site in TESTING - Please DO NOT Enter Actual Payment information!!!");
+      setShowAlert(false); // Disable alert after showing
+    }
+  }, [showAlert]);
+  
   return (
     <div className="mb-6">
       <h2 className="text-2xl font-semibold mb-2">Payment Information</h2>
+      <h1>Page in Testing Status - Please do not enter actual payment info!!!</h1>
       <input
         type="text"
         name="vendorCode"
