c7f17376b58b2d48bb72e8eddf7ee278e396d1a7
rukaiah-edhah <rukaiah.edhah@yahoo.com>
2024-09-15 19:22:44 -0400

    Added analytics :)

--- ijele/app/layout.tsx
+++ ijele/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { CartProvider } from "@/components/Payment/cartProvider";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
   return (
     <CartProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          {children}
+          <Analytics />
+        </body>
       </html>
     </CartProvider>
   );
